6db7d2aef664169a76765082a805f383a42da234
Admin <Admin@ADMINRG-PK096SO>
2020-02-26 23:49:30 +0530

    CityController will acts as a source in event driven process

---
+++ Annotation/src/com/messagedriven/annotation/CityController.java
@@ -0,0 +1,25 @@
+package com.messagedriven.annotation;
+
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.ApplicationEventPublisherAware;
+
+public class CityController implements ApplicationEventPublisherAware{
+
+	ApplicationEventPublisher publisher;
+	ReloadEvent reload;
+	
+	public void addCity(int cityNo,String cityName)
+	{
+		System.out.println("Adding the City");
+		reload=new ReloadEvent("cities");
+		reload.setTable("cities");
+		publisher.publishEvent(reload);	
+	}
+	
+	@Override
+	public void setApplicationEventPublisher(ApplicationEventPublisher publisher) {
+		this.publisher = publisher;
+		
+	}
+
+}
